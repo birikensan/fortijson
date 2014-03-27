74fdafd0ec8b2fec574adb07c9bf79f5030f6188
kongou-ae <y.matsumoto.ae@gmail.com>
2014-03-27 02:47:04 -0400

    update

--- fortijson.py
+++ fortijson.py
@@ -4,20 +4,28 @@
 # vdomには対応していません。
 # vdomで利用したい場合は、各vdomの設定項目を別ファイルに分けて下さい。
 #
+# config firewall policy の中に再度configが出てくるコンフィグにも対応していません。
+# 例：identity-based-policy
+
+#
 import re
 import linecache
+import json
+import time
+
+configfile = "config.conf"
 
 # config firewall policyの場所を探して変数topに格納する。
 top = 0
 regex_top = re.compile('^config firewall policy$')
-for i,line in enumerate(open('config.conf', 'r')):
+for i,line in enumerate(open(configfile, 'r')):
     if regex_top.search(line) is not None:
         top = i + 1 
 
 # endの場所を探して、配列endに格納する。
 end = []
 regex_end = re.compile('^end$')
-for i,line in enumerate(open('config.conf', 'r')):
+for i,line in enumerate(open(configfile, 'r')):
     if regex_end.search(line) is not None:
         end.append(i + 1)
 
@@ -45,7 +53,7 @@ config.append('"config firewall policy": {')
 # topからlastの行番号の範囲で
 for linenum in range(top, last + 1):
     # 行番号を指定して行を読み込み後続処理に進む。
-    line = linecache.getline('config.conf',linenum)
+    line = linecache.getline(configfile,linenum)
     
     # edit hogehogeを変換する部分
     if regex_id.search(line) is not None:
@@ -71,7 +79,7 @@ for linenum in range(top, last + 1):
         config.append("}")
         
         
-    
+# 微調整する
 for i in range(len(config)):
     #
     # 文字列が},だったらひとつ前の配列の末尾から,を削除する。
@@ -83,5 +91,51 @@ for i in range(len(config)):
 
 config.append('}')
 
+# 配列の要素を一つの変数に格納する
+tmp = ''
 for line in config:
-    print(line)
+    tmp = tmp + line
+
+# 格納した変数（str）をjsonモジュールで辞書型に変更する。
+json_conf = json.loads(tmp)
+
+# ポリシーで設定されている設定項目を抽出する。
+# ポリシーidの箇所をイテレータで確認する。
+params = []
+for id in iter(json_conf['config firewall policy'].keys()):
+    # ポリシーID配下の設定項目をイテレータで確認する
+    for value in iter(json_conf['config firewall policy'][id].keys()):
+        # 確認した要素を配列に格納する。
+        params.append(value)
+
+#　配列から重複を削除し、ユニークな設定項目配列を作成する。
+param_array = list(set(params))
+
+# ポリシーID配下の辞書の中身をチェックし、標準出力する。
+# ポリシーIDを一つずつ変数idに格納する。
+policy_array = []
+policy_line = ''
+for id in iter(json_conf['config firewall policy'].keys()):
+    # 設定項目の配列から一つずつ設定項目をparamに格納する。
+    for param in param_array:
+        # 辞書の中にparamの設定内容があるかチェックして、pilicy_lineに要素を追記していく
+        policy_line = policy_line + ',' + json_conf['config firewall policy'][id].get(param,"-")
+        #print(param + ":" + json_conf['config firewall policy'][id].get(param,"-"))
+    #　追記した要素をpolicy_array配列に格納する。
+    policy_array.append(id+policy_line)
+    # 次のIDに備えてpolicy_lineを初期化する
+    policy_line = ''
+
+#　ポリシー一覧を出力するために、1行目を出力する
+param_line = 'id'
+for param in param_array:
+    param_line = param_line + "," + param
+
+f = open('parameter.txt','w')
+
+f.write(param_line+"\n")
+policy = ''
+for policy in policy_array:
+    f.write(policy+"\n")
+
+f.close
