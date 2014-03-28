f5e2443343cc50c347fb25e47f6636d4858bbc04
kongou-ae <y.matsumoto.ae@gmail.com>
2014-03-28 20:56:09 +0900

    コメントを追加

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+﻿#/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import fortijson
@@ -7,37 +7,41 @@ from bottle import route, request, run, static_file, default_app
 from bottle import jinja2_template as template
 import tempfile
 
-
+# サブディレクトリ/fortiで公開する
 @route("/forti")
 def index():
     return template('index')
     
-
+# configファイルをPOSTした後の処理
 @route("/forti", method='POST')
 def upload():
-    configfile = request.files.get('file')
-    
-    # policytojsonで読み込むためのtempファイルを作成する。
+    # Formからファイルを受け取る。
+    configfile = request.files.get('file')    
+    # ファイルをpolicytojsonで読み込むためのtempファイルを作成する。
     f = tempfile.NamedTemporaryFile()
     strline = ''
-    # HTMLから受けったファイルがbyteなので、一行ずつ文字列に変換して変数に格納する
+    # HTMLから受けったファイルがbyteなので、一行ずつ文字列に変換して変数strlineに格納する
     # もっといい方法がありそう。。。
     for line in configfile.file.readlines():
         strline = strline + line.decode()
     # byteに戻してtempファイルに書き込み
     f.write(strline.encode())
-    # tempファイルのパスをメソッドに渡して、jsonな辞書型にする。
+    # tempファイルのパスをpolicytojsonメソッドに渡して、jsonな辞書型を受け取る。
     policyjson = fortijson.policytojson(f.name)
+
     # 出力先ファイル名用の時刻を取得する$
     now = str(datetime.datetime.now())
+    # 返還後のファイル名をもともとのファイル名+_時刻とする。
     filename = configfile.filename+"_"+now
+    # tmpfileをクローズする。
     f.close
 
-    # 辞書と時刻を引数に関数を呼び出す$
+    # 辞書と時刻を引数にjsontoparamメソッド呼び出して、ファイルを作成する。
     fortijson.jsontoparam(policyjson,filename)
 
     return template('index',filename=filename)
 
+# 作成されたファイルを公開するための処理
 @route('/forti/static/<filename>')
 def static(filename):
     return static_file(filename, root='./static')
